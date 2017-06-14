362f8073cb0a0c910c7cca51673152e25fd4f273
Anran <maoanran@gmail.com>
2017-06-14 09:15:07 +0800

    o(n)

--- main.cpp
+++ main.cpp
@@ -5,31 +5,32 @@ using namespace std;
 
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int pos = 0;
-        for (int n : nums) {
-            if (n != val) {
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 1) {
+            return 0;
+        }
+        int pos = 1;
+        int last = nums[0];
+        for(int n : nums) {
+            if(n != last) {
                 nums[pos++] = n;
+                last = n;
             }
         }
-        int result = pos;
-        while (pos < nums.size()) {
-            nums[pos++] = val;
-        }
-        return result;
+        return pos;
     }
 };
 
 int main() {
     Solution *s = new Solution();
-    vector<int> v{3, 2, 2, 3, 12};
-    int result = s->removeElement(v, 3);
+    vector<int> v{2, 2, 3, 3, 12};
+    int result = s->removeDuplicates(v);
 
     for (int n : v) {
         cout << n << endl;
     }
 
-    cout << result << endl;
+    cout << "result" << result << endl;
 
     return 0;
 }
