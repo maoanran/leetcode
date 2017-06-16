c0236306eb99db7057d4447b9e396f84a9a55f9d
Anran <maoanran@gmail.com>
2017-06-16 08:38:30 +0800

    lg(n)

--- main.cpp
+++ main.cpp
@@ -4,26 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if (x == 0) {
-            return 0;
+    bool isPerfectSquare(int num) {
+        long x = num;
+        while (x * x > num) {
+            x = (x + num / x) / 2;
         }
-        if (n == 0)
-            return 1;
-        if (n == INT_MIN) {
-            return 1 / (x * x * myPow(x * x, (n + 2) / -2));
-        }
-        if (n < 0) {
-            x = 1 / x;
-            n = -n;
-        }
-        return n & 1 ? x * myPow(x * x, n / 2) : myPow(x * x, n / 2);
+        return x * x == num;
     }
 };
 
 int main() {
     Solution *s = new Solution();
-    double v = s->myPow(2.00000, -2147483648);
+    double v = s->isPerfectSquare(20623316);
     cout << v << endl;
     return 0;
 }
