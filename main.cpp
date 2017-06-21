08514488e14b8c6b6da0961701d8cbddc05cb5cc
Anran <maoanran@gmail.com>
2017-06-21 23:49:20 +0800

    o(lgN)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,58 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    bool isPerfectSquare(int num) {
-        long x = num;
-        while (x * x > num) {
-            x = (x + num / x) / 2;
+    int search(vector<int> &nums, int target) {
+        if (nums.size() == 0) {
+            return -1;
         }
-        return x * x == num;
+        int l = 0, h = nums.size() - 1;
+
+        // find smallest
+        while (l < h) {
+            int m = (l + h) / 2;
+            if (nums[m] < nums[h]) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        cout << "smallest num:" <<  nums[l] << endl;
+
+        if (target >= nums[l] && target <= nums[nums.size() - 1]) {
+            h = nums.size() - 1;
+        } else {
+            l = 0;
+        }
+
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (target > nums[m]) {
+                l = m + 1;
+            } else if (target < nums[m]) {
+                h = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
     }
 };
 
 int main() {
     Solution *s = new Solution();
-    double v = s->isPerfectSquare(20623316);
+//    vector<int> nums{4, 5, 6, 0, 1, 2};
+//    vector<int> nums{0, 1, 2, 3, 4};
+//    vector<int> nums{1, 2, 3, 4, 0};
+//    vector<int> nums{4, 0, 1, 2, 3};
+//    vector<int> nums{0, 4};
+//    vector<int> nums{4, 0};
+//    vector<int> nums{1};
+    vector<int> nums{};
+    int v = s->search(nums, 0);
     cout << v << endl;
     return 0;
 }
