cf1b17ffea502f406deede60f6cb6fc773669f58
Anran <maoanran@gmail.com>
2017-06-22 21:54:29 +0800

    o(lgN)

--- main.cpp
+++ main.cpp
@@ -12,30 +12,29 @@ public:
         int l = 0, h = nums.size() - 1;
 
         // find smallest
-        while (l < h) {
-            int m = (l + h) / 2;
-            if (nums[m] < nums[h]) {
-                h = m;
-            } else {
-                l = m + 1;
-            }
-        }
-        cout << "smallest num:" <<  nums[l] << endl;
-
-        if (target >= nums[l] && target <= nums[nums.size() - 1]) {
-            h = nums.size() - 1;
-        } else {
-            l = 0;
-        }
-
         while (l <= h) {
             int m = (l + h) / 2;
-            if (target > nums[m]) {
-                l = m + 1;
-            } else if (target < nums[m]) {
-                h = m - 1;
-            } else {
+
+            if (target == nums[m]) {
                 return m;
+            } else if (target == nums[h]) {
+                return h;
+            } else if (target == nums[l]) {
+                return l;
+            }
+
+            if (nums[m] < nums[h]) {
+                if (nums[m] < target && target < nums[h]) {
+                    l = m + 1;
+                } else {
+                    h = m - 1;
+                }
+            } else {
+                if (nums[l] < target && target < nums[m]) {
+                    h = m - 1;
+                } else {
+                    l = m + 1;
+                }
             }
         }
         return -1;
@@ -46,7 +45,7 @@ int main() {
     Solution *s = new Solution();
 //    vector<int> nums{4, 5, 6, 0, 1, 2};
 //    vector<int> nums{0, 1, 2, 3, 4};
-//    vector<int> nums{1, 2, 3, 4, 0};
+//    vector<int> nums{ 2, 3, 4, 0, 1};
 //    vector<int> nums{4, 0, 1, 2, 3};
 //    vector<int> nums{0, 4};
 //    vector<int> nums{4, 0};
